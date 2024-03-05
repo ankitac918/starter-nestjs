24b4f7827a6e745928cdeb0ab82b56d973c3a6ec
ankitac918 <106753986+ankitac918@users.noreply.github.com>
2024-03-05 12:37:19 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'kya re bhikhmangyaaa';
+    return 'chala ja ....';
   }
 }
