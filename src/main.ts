5e658e1cf15ff3ce1d5a962217ab4f1bebcd4796
ankitac918 <106753986+ankitac918@users.noreply.github.com>
2024-03-19 17:37:02 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000,'192.168.1.97');
+  await app.listen(8080,'192.168.1.97');
 }
 bootstrap();
